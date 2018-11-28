a6e3a68f0eab6643540a041d417573e85e11faf4
肖一凡 <xyf@mac23.local>
2018-11-28 10:34:40 +0800

    Initial Commit

---
+++ xyf11.28/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  xyf11.28
+//
+//  Created by XYF on 2018/11/28.
+//  Copyright © 2018 XYF. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ xyf11.28Tests/xyf11_28Tests.swift
@@ -0,0 +1,34 @@
+//
+//  xyf11_28Tests.swift
+//  xyf11.28Tests
+//
+//  Created by XYF on 2018/11/28.
+//  Copyright © 2018 XYF. All rights reserved.
+//
+
+import XCTest
+@testable import xyf11_28
+
+class xyf11_28Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
