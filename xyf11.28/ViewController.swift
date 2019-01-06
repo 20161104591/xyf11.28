5643d0741717f95a45c415ed2b5463e1a376f531
20161104591 <346680424@qq.com>
2019-01-06 18:47:55 +0800

    1
    1

--- xyf11.28/ViewController.swift
+++ xyf11.28/ViewController.swift
@@ -7,11 +7,73 @@
 //
 
 import UIKit
-
+import LocalAuthentication
 class ViewController: UIViewController {
+    func errorMessageForLAErrorCode(errorCode: Int) -> String {
+        var message = ""
+        
+        switch errorCode {
+        case LAError.appCancel.rawValue:
+            message = "Authentication was cancelled by application"
+            
+        case LAError.authenticationFailed.rawValue:
+            message = "The user failed to provide valid credentials"
+            
+        case LAError.invalidContext.rawValue:
+            message = "The context is invalid"
+            
+        case LAError.passcodeNotSet.rawValue:
+            message = "Passcode is not set on the device"
+            
+        case LAError.systemCancel.rawValue:
+            message = "Authentication was cancelled by the system"
+            
+        case LAError.touchIDLockout.rawValue:
+            message = "Too many failed attempts."
+            
+        case LAError.userCancel.rawValue:
+            message = "The user did cancel"
+            
+        case LAError.userFallback.rawValue:
+            message = "The user chose to use the fallback"
+            
+        default:
+            message = "Did not find error code on LAError object"
+        }
+        return message
+    }
+    
+    func touchID (){
+        let context = LAContext()
+        var error: NSError?
+        
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            
+            // 开始进入识别状态，以闭包形式返回结果。闭包的 success 是布尔值，代表识别成功与否。error 为错误信息。
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: "请用指纹解锁", reply: {success, error in
+                
+                if success {
+                    // 成功之后的逻辑， 通常使用多线程来实现跳转逻辑。
+                }else {
+                    if let error = error as? NSError {
+                        // 获取错误信息
+                        let message = self.errorMessageForLAErrorCode(errorCode: error.code)
+                        print(message)
+                    }
+                }
+            })
+        }
+    }
+    
+   
+    @IBAction func touchID(_ sender: Any) {
+        touchID()
+    }
+   
+  
     @IBOutlet weak var result: UILabel!
     var result_1 = ""
-    var re = 0
+    var re = 1
     var add = 0
     var number = 0
     var judge = 0
@@ -33,13 +95,41 @@ class ViewController: UIViewController {
     }
     @IBAction func plus(_ sender: Any) {
         if add == 1{
+            var c = 0.0
             let a = Double(result_1)!
             let b = Double(result.text!)!
-            let c = a + b
+            if number == 1{
+                c = ((a * 1000000) + (b * 1000000))/1000000
+                result.text = String(c)
+            }
+            if number == 2{
+                c = ((a * 1000000) - (b * 1000000))/1000000
+                result.text = String(c)
+            }
+            if number == 3{
+                c = ((a * 1000000) * (b * 1000000))/1000000000000
+                result.text = String(c)
+            }
+            if number == 4{
+                c = (a * 1000000) / (b * 1000000)
+                result.text = String(c)
+            }
+            result.text = String(c)
+            var xyf:String = result.text!
+            while xyf.last == "0"{
+                xyf.removeLast()
+                print(xyf)
+            }
+            while xyf.last == "."{
+                xyf.removeLast()
+                print(xyf)
+            }
+            result.text = xyf
             result_1 = String(c)
-            result.text = result_1
+            judge = 0
             number = 1
             re = 1
+            
         }
         else{
             if result.text == ""{
@@ -49,17 +139,44 @@ class ViewController: UIViewController {
             let x = result.text!
             result_1 = String(x)
             result.text = result_1
+            judge = 0
             number = 1
             re = 1
         }
     }
     @IBAction func subtruct(_ sender: Any) {
         if add == 1{
+            var c = 0.0
             let a = Double(result_1)!
             let b = Double(result.text!)!
-            let c = a - b
+            if number == 1{
+                c = ((a * 1000000) + (b * 1000000))/1000000
+                result.text = String(c)
+            }
+            if number == 2{
+                c = ((a * 1000000) - (b * 1000000))/1000000
+                result.text = String(c)
+            }
+            if number == 3{
+                c = ((a * 1000000) * (b * 1000000))/1000000000000
+                result.text = String(c)
+            }
+            if number == 4{
+                c = (a * 1000000) / (b * 1000000)
+                result.text = String(c)
+            }
+            result.text = String(c)
+            var xyf:String = result.text!
+            while xyf.last == "0"{
+                xyf.removeLast()
+                print(xyf)
+            }
+            while xyf.last == "."{
+                xyf.removeLast()
+                print(xyf)
+            }
+            result.text = xyf
             result_1 = String(c)
-            result.text = result_1
             number = 2
             re = 1
         }
@@ -77,11 +194,37 @@ class ViewController: UIViewController {
     }
     @IBAction func multiply(_ sender: Any) {
         if add == 1{
+            var c = 0.0
             let a = Double(result_1)!
             let b = Double(result.text!)!
-            let c = a * b
+            if number == 1{
+                c = ((a * 1000000) + (b * 1000000))/1000000
+                result.text = String(c)
+            }
+            if number == 2{
+                c = ((a * 1000000) - (b * 1000000))/1000000
+                result.text = String(c)
+            }
+            if number == 3{
+                c = ((a * 1000000) * (b * 1000000))/1000000000000
+                result.text = String(c)
+            }
+            if number == 4{
+                c = (a * 1000000) / (b * 1000000)
+                result.text = String(c)
+            }
+            result.text = String(c)
+            var xyf:String = result.text!
+            while xyf.last == "0"{
+                xyf.removeLast()
+                print(xyf)
+            }
+            while xyf.last == "."{
+                xyf.removeLast()
+                print(xyf)
+            }
+            result.text = xyf
             result_1 = String(c)
-            result.text = result_1
             number = 3
             re = 1
         }
@@ -99,11 +242,37 @@ class ViewController: UIViewController {
     }
     @IBAction func divide(_ sender: Any) {
         if add == 1{
+            var c = 0.0
             let a = Double(result_1)!
             let b = Double(result.text!)!
-            let c = a / b
+            if number == 1{
+                c = ((a * 1000000) + (b * 1000000))/1000000
+                result.text = String(c)
+            }
+            if number == 2{
+                c = ((a * 1000000) - (b * 1000000))/1000000
+                result.text = String(c)
+            }
+            if number == 3{
+                c = ((a * 1000000) * (b * 1000000))/1000000000000
+                result.text = String(c)
+            }
+            if number == 4{
+                c = (a * 1000000) / (b * 1000000)
+                result.text = String(c)
+            }
+            result.text = String(c)
+            var xyf:String = result.text!
+            while xyf.last == "0"{
+                xyf.removeLast()
+                print(xyf)
+            }
+            while xyf.last == "."{
+                xyf.removeLast()
+                print(xyf)
+            }
+            result.text = xyf
             result_1 = String(c)
-            result.text = result_1
             number = 4
             re = 1
         }
@@ -122,6 +291,7 @@ class ViewController: UIViewController {
     @IBAction func dot(_ sender: Any) {
         if re == 1{
             result.text = "0."
+            judge = 1
             re = 0
         }
         if judge == 1{
@@ -129,7 +299,8 @@ class ViewController: UIViewController {
         }
         else{
             if result.text == ""{
-                result.text = ""
+                result.text = "0."
+                judge = 1
             }
             else{
                 result.text = result.text! + "."
@@ -226,21 +397,21 @@ class ViewController: UIViewController {
             let b = Double(result.text!)!
             var c = 0.0
             if number == 1{
-                c = a + b
+                c = ((a * 1000000) + (b * 1000000))/1000000
                 result.text = String(c)
             }
             if number == 2{
-                c = a - b
+                c = ((a * 1000000) - (b * 1000000))/1000000
                 result.text = String(c)
             }
             if number == 3{
-                c = a * b
+                c = ((a * 1000000) * (b * 1000000))/1000000000000
                 result.text = String(c)
             }
             if number == 4{
-                c = a / b
+                c = (a * 1000000) / (b * 1000000)
                 result.text = String(c)
-            }
+            } 
             result.text = String(c)
             var xyf:String = result.text!
             while xyf.last == "0"{
